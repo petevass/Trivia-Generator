0a3e6a4763c30fa47843038d780ecf6eb82b4495
petevass <pvassiliou10@gmail.com>
2026-07-13 12:55:29 -0400

    initial commit, all generated files added

---
+++ src/main/java/com/stardance/triviagenerator/TriviaGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.stardance.triviagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stardance/triviagenerator/TriviaGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardance.triviagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
